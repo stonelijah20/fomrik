0a779b497d5553cea21fa60765bbd187525ce66f
stonelijah21 <80172565+stonelijah21@users.noreply.github.com>
2022-06-06 03:26:12 +0100

    ...

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FormikForm } from "./components/Formik";
+
+function App() {
+  return (
+    <div className="App">
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Formik.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { useFormik } from "formik";
+
+export const FormikForm = () => {
+  // Note that we have to initialize ALL of fields with values. These
+  // could come from props, but since we don’t want to prefill this form,
+  // we just use an empty string. If we don’t do this, React will yell
+  // at us.
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      number: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+      />
+
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+      />
+
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+
+      <label htmlFor="email">Phone Number</label>
+      <input
+        id="number"
+        name="number"
+        type="phone"
+        onChange={formik.handleChange}
+        value={formik.values.number}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
